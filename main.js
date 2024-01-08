bfb1c62ccfea789d27a8668bdba2661d7c681650
Casper0x413 <casper0x413@gmail.com>
2024-01-08 18:59:27 -0500

    Improved basic functionality

--- main.js
+++ main.js
@@ -13,34 +13,50 @@ const platforms = {
   }
 };
 
-const route = new URL( location );
+class Livestream {
+  constructor( platform , tag ) {
+    platform = String( platform ).toLowerCase();
 
-for ( const [ platform , streams ] of route.searchParams.entries() ) {
-  if ( !platforms?.[ platform.toLowerCase() ] ) continue;
+    this.platform = platform in platforms ? platform : "";
+    this.tag = String( tag );
 
-  const stream = streams.split( " " );
+    if ( !this.platform ) throw new Error( "Livestream() requires a supported platform" );
+    if ( !this.tag ) throw new Error( "Livestream() requires a non empty tag" );
 
-  stream.forEach( tag => {
+    this.routes = {
+      "chat" : platforms[ this.platform ].chat.replace( "{tag}", this.tag ),
+      "video" : platforms[ this.platform ].video.replace( "{tag}", this.tag )
+    };
+
+    return this.#embed();
+  }
+
+  #embed() {
     const div = document.createElement( "div" );
 
-    const chatRoute = platforms[ platform ].chat.replace( "{tag}" , tag );
-    const videoRoute = platforms[ platform ].video.replace( "{tag}" , tag );
-
     const chat = document.createElement( "iframe" );
-    chat.id = "chat";
-    chat.src = chatRoute;
+    chat.src = this.routes.chat;
     chat.height = "480px";
     chat.width = "720px";
 
     const video = document.createElement( "iframe" );
-    video.id = "video";
-    video.src = videoRoute;
+    video.src = this.routes.video;
     video.height = "480px";
     video.width = "720px";
 
     div.appendChild( video );
     div.appendChild( chat );
 
-    document.body.appendChild( div );
+    return div;
+  }
+}
+
+const route = new URL( location );
+
+for ( const [ platform , streams ] of route.searchParams.entries() ) {
+  const stream = streams.split( " " );
+
+  stream.forEach( tag => {
+    document.body.appendChild( new Livestream( platform , tag ) );
   } );
 }
